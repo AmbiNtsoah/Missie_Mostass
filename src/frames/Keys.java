0beff2fd85dc41b490d8a7e5a2d6e1f8d72835d5
MiSantatra <miharimbola.ramanantsoa@gmail.com>
2025-03-27 08:24:08 +0300

    classe keys

--- src/frames/Keys.java
+++ src/frames/Keys.java
@@ -1,5 +1,28 @@
 package frames;
 
 public class Keys {
+    private int id;
+    private String key;
 
+    public Keys(int id, String key) {
+        this.id = id;
+        this.key = key;
+    }
+
+    // Getters et setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
 }
