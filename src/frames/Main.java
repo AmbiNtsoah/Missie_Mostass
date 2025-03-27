022c2076cbcdfcb311d9cbeb18397467d879ad0c
MiSantatra <miharimbola.ramanantsoa@gmail.com>
2025-03-27 07:50:25 +0300

    suppression commentaires

--- src/frames/Main.java
+++ src/frames/Main.java
@@ -2,10 +2,7 @@ package frames;
 
 import java.awt.EventQueue;
 
-/**
- * Point d'entrée pour permettre à notre
- * interface graphique de s'afficher
- */
+
 public class Main {
 	/**
 	 * Methode consulté par le compilateur JAVA
